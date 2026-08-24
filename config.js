88d6f4c913e616739444a9ab21634c4b60998bb7
Caua Fernandes Jorge <cauafj008@gmail.com>
2026-08-24 18:09:02 -0300

    Adiciona configuração Firebase

---
+++ config.js
@@ -0,0 +1,11 @@
+// Configuração pública do app Web no Firebase.
+// A segurança dos dados é controlada pelo Firebase Authentication + regras do Firestore.
+window.MEDLEMBRETE_FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyA669oq_oN2anxhpRYSj7NNdeCG3HrXyJQ',
+  authDomain: 'medlembrete-d265e.firebaseapp.com',
+  projectId: 'medlembrete-d265e',
+  storageBucket: 'medlembrete-d265e.firebasestorage.app',
+  messagingSenderId: '980003685280',
+  appId: '1:980003685280:web:cc988927adbd066e559ac5',
+  measurementId: 'G-SYNRDJKZZZ'
+};
