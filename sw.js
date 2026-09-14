925ac0ffbe41290460eadba50c9b19a6ebf10098
Caua Fernandes Jorge <cauafj008@gmail.com>
2026-09-14 18:15:12 -0300

    Atualiza cache do PWA para nova interface

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='medlembrete-v7';
+const CACHE='medlembrete-v8';
 const ASSETS=['/','/index.html','/styles.css','/app.js','/config.js','/manifest.json','/icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
